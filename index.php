7d2dd42c88a2974ba0e14a8ca5dd5e37d766a1bc
Sebbi3000 <sbi3wa@gmail.com>
2018-07-27 16:47:59 +0200

    nouveau fichier index

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>1st doc on github</title>
+</head>
+	<body>
+		<article>
+			<header>
+				<h1>1st article on gibhub</h1>
+				<p>la ligne d'accroche de cet article</p>
+			</header>
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</section>
+		</article>
+</body>
+</html>
